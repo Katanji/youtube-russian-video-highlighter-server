810ca80a24037132ac63b5f8570f51a2d9676243
rozhkov <dmitry.r@zlogic.io>
2024-07-28 15:16:29 -0400

    added default route

--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,8 @@
 use App\Http\Controllers\ChannelController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function () {
+    return view('welcome');
+});
+
 Route::post('/check-channels', [ChannelController::class, 'checkChannels'])->name('check-channels');
